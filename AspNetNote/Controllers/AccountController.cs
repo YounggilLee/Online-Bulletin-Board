8d90eba4b4552b17d631c1438192366eefaa1349
Younggil Lee <leyoungg@sheridancollege.ca>
2017-07-21 16:17:59 -0400

    Created datable using ASP.NET entity framwork core in sql database Implemant Register page

---
+++ AspNetNote/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetNote.Models;
+using AspNetNote.DataContext;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetNote.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspNetNoteContext())
+                {
+                    db.Users.Add(model);  //upload data to memory
+                    db.SaveChanges();  //save data to sql table
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+    }
+}
